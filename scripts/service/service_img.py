631940bce053ec83be8c2239fd6add6e4eacbb90
kaar <flyingdeer000000@gmail.com>
2024-04-10 19:14:11 +0800

    commit

---
+++ scripts/service/service_img.py
@@ -0,0 +1,159 @@
+import time
+from pathlib import Path
+
+from PIL import Image
+import rembg
+import os
+import glob
+
+
+def file_count(target, suffix="*.png"):
+    pattern = os.path.join(target, suffix)
+    files = glob.glob(pattern)
+    return len(files)
+
+
+def remove_background(
+        rem_src_dir: str,
+        rem_des_dir: str,
+        session,
+):
+    if session is None:
+        return
+
+    print("[rembg] {} ---> {}".format(rem_src_dir, rem_des_dir))
+
+    os.makedirs(rem_des_dir, mode=777, exist_ok=True)
+
+    files = Path(rem_src_dir).glob('*.[pP][nN][gG]')
+
+    index = 0
+    total = file_count(rem_src_dir)
+    for file in files:
+        input_path = str(file)
+        output_path = str(rem_des_dir + os.path.sep + (file.stem + file.suffix))
+
+        with open(input_path, 'rb') as i:
+            with open(output_path, 'wb') as o:
+                data_in = i.read()
+                data_out = rembg.remove(data_in, session=session)
+                o.write(data_out)
+                index = index + 1
+                print("[rembg] [{}/{}] {}".format(index, total, output_path))
+
+    if total > 0:
+        print("")
+
+
+def resize(input_path, output_path, to_width=512, to_height=512):
+    image = Image.open(input_path)
+    ratio = image.width / image.height
+    to_ratio = to_width / to_height
+
+    if ratio > to_ratio:
+        new_width = to_width
+        new_height = round(new_width / ratio)
+    else:
+        new_height = to_height
+        new_width = round(new_height * ratio)
+
+    # Resize the image while maintaining the aspect ratio
+    resized_image = image.resize((new_width, new_height))
+
+    # Create a new image with the desired dimensions and transparent background
+    padded_image = Image.new("RGBA", (to_width, to_height), (0, 0, 0, 0))
+
+    # Calculate the padding offsets
+    x_offset = (to_width - new_width) // 2
+    y_offset = (to_height - new_height) // 2
+
+    # Paste the resized image onto the padded image with transparent pixels
+    padded_image.paste(resized_image, (x_offset, y_offset))
+
+    # Save the padded image with transparent pixels
+    padded_image.save(output_path)
+
+
+def resize_directory(resize_src_dir, resize_des_dir, width=512, height=512):
+    print("[resize] {} ---> {} ".format(resize_src_dir, resize_des_dir))
+
+    if width <= 0:
+        width = 512
+
+    os.makedirs(resize_des_dir, mode=777, exist_ok=True)
+
+    index = 0
+    total = file_count(resize_src_dir)
+    for filename in os.listdir(resize_src_dir):
+        if filename.lower().endswith('.png'):
+            image_path = os.path.join(resize_src_dir, filename)
+            output_path = os.path.join(resize_des_dir, filename)
+            resize(image_path, output_path, width, height)
+            index = index + 1
+            print("[resize] [{}/{}] {} ".format(index, total, output_path))
+
+    if total > 0:
+        print("")
+
+
+def process(
+        src_dir,
+        des_dir,
+        r_width=512, r_height=512,
+        resize_exec=True,
+        rembg_model="",
+        recursive_depth=None
+):
+    start_time = time.time()
+
+    try:
+        src_dir = str(src_dir)
+        des_dir = str(des_dir)
+        r_width = int(r_width)
+        r_height = int(r_height)
+        resize_exec = bool(resize_exec)
+        rembg_model = str(rembg_model)
+        recursive_depth = int(recursive_depth)
+
+        if r_width <= 0:
+            r_width = 512
+        if r_height <= 0:
+            r_height = 512
+
+        os.makedirs(des_dir, mode=0o777, exist_ok=True)
+
+        sep_count = src_dir.count(os.path.sep)
+
+        if rembg_model == "def" or rembg_model == "default":
+            rembg_model = "isnet-anime"
+
+        if rembg_model == "" or rembg_model == "none" or rembg_model == "null":
+            rembg_session = None
+        else:
+            rembg_session = rembg.new_session(model_name=rembg_model)
+
+        for root, dirs, files in os.walk(src_dir):
+            # Calculate the current depth
+            depth = root.count(os.path.sep) - sep_count
+
+            # Skip if max_depth is specified and the current depth exceeds it
+            if recursive_depth is not None and depth > recursive_depth:
+                print("[process] max recursive depth reached {} > {}".format(depth, recursive_depth))
+                continue
+
+            dir_name = os.path.basename(root)
+            des_path = str(os.path.join(des_dir, dir_name))
+
+            print("[process] root: {}, depth: {}, max depth: {}".format(root, depth, recursive_depth))
+            print("[process] name: {}, to: {}".format(dir_name, des_path))
+
+            if rembg_session is None:
+                if resize_exec:
+                    resize_directory(root, des_path, r_width, r_height)
+            else:
+                remove_background(root, des_path, rembg_session)
+                if resize_exec:
+                    resize_directory(des_path, des_path, r_width, r_height)
+    finally:
+        elapsed_time = time.time() - start_time
+        print(f"[process] elapsed time: {elapsed_time} seconds")
---
+++ scripts/ui_tab.py
@@ -0,0 +1,137 @@
+import traceback
+
+import gradio as gr
+
+import modules.scripts as scripts
+from modules import script_callbacks
+from modules.shared import opts
+
+from scripts.service import service_img
+
+FN_GO_RUNNING = False
+
+
+class Script(scripts.Script):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def title(self):
+        return "Flyer"
+
+    def show(self, is_img2img):
+        return scripts.AlwaysVisible
+
+    def ui(self, is_img2img):
+        return ()
+
+
+def fn_go(
+        dir_src,
+        dir_des,
+        num_depth,
+        rembg_model,
+        is_resize,
+        resize_width,
+        resize_height
+):
+    global FN_GO_RUNNING
+
+    print("[img process] here {}".format(FN_GO_RUNNING))
+
+    if FN_GO_RUNNING:
+        print("[img process] running")
+        return
+
+    FN_GO_RUNNING = True
+    try:
+        if dir_src == "" or dir_des == "":
+            print("[img process] miss params: input directory & output directory")
+            return
+
+        print("[img process] image directory {} -> {}".format(dir_src, dir_des))
+        print("[img process] remove image background model {}".format(rembg_model))
+        if is_resize:
+            print("[img process] image resize {} x {}".format(resize_width, resize_height))
+        else:
+            print("[img process] image keep original size")
+
+        service_img.process(
+            src_dir=str(dir_src),
+            des_dir=str(dir_des),
+            r_width=int(resize_width),
+            r_height=int(resize_height),
+            resize_exec=bool(is_resize),
+            rembg_model=str(rembg_model),
+            recursive_depth=int(num_depth),
+        )
+    except:
+        traceback.print_exc()
+    finally:
+        FN_GO_RUNNING = False
+        print("[img process] done")
+    return []
+
+
+def on_ui_tabs():
+    with gr.Blocks(analytics_enabled=False) as flyer_editor:
+        with gr.Row():
+            with gr.Column():
+                with gr.Row():
+                    textbox_dir_src = gr.Textbox(
+                        label='Input Directory',
+                        placeholder='/path/input/images or /path/input/images/**/*'
+                    )
+                with gr.Row():
+                    textbox_dir_des = gr.Textbox(
+                        label='Output Directory',
+                        placeholder='/path/output/images or /path/output/images/**/*'
+                    )
+                with gr.Row():
+                    number_recursive_depth = gr.Number(
+                        value=0,
+                        label='Directory Recursive Depth'
+                    )
+                with gr.Row():
+                    dropdown_rembg_model = gr.Dropdown(
+                        label="Remove Background Model  "
+                              "| 'none' for no removing  "
+                              "| first time executing takes a while  ",
+                        value="isnet-anime",
+                        choices=[
+                            "none",
+                            "u2net",
+                            "u2netp",
+                            "u2net_human_seg",
+                            "u2net_cloth_seg",
+                            "silueta",
+                            "isnet-general-use",
+                            "isnet-anime",
+                            "sam"
+                        ]
+                    )
+                with gr.Row():
+                    checkbox_resize = gr.Checkbox(
+                        value=True,
+                        label="Image Resize"
+                    )
+                    slider_w = gr.Slider(label="resize width", minimum=64, maximum=2048, value=512, step=64,
+                                         interactive=True)
+                    slider_h = gr.Slider(label="resize height", minimum=64, maximum=2048, value=512, step=64,
+                                         interactive=True)
+                with gr.Row():
+                    button_go = gr.Button(value="GO!", variant="primary")
+
+        button_go.click(fn_go, [
+            textbox_dir_src,
+            textbox_dir_des,
+            number_recursive_depth,
+            dropdown_rembg_model,
+            checkbox_resize,
+            slider_w,
+            slider_h,
+        ], [])
+
+    return [(flyer_editor, "Flyer", "flyer_editor")]
+
+
+script_callbacks.on_ui_tabs(on_ui_tabs)
