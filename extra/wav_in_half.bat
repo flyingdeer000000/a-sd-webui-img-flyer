cf04a32dba2e27b1332756e9311479a2075b2851
kaar <flyingdeer000000@gmail.com>
2024-05-02 17:22:53 +0800

    commit

--- extra/wav_in_half.bat
+++ extra/wav_in_half.bat
@@ -9,12 +9,22 @@ if "%input_dir%"=="" (
 )
 
 if "%output_dir%"=="" (
-    echo Output directory not provided.
-    exit /b
+    set "output_dir=%input_dir%\output"
 )
 
 mkdir "%output_dir%" 2>nul
 
 for %%F in ("%input_dir%\*.wav") do (
-    ffmpeg -i "%%F" -filter_complex "[0:a]asplit=2[out1][out2]" -map "[out1]" "%output_dir%\%%~nF_part1.wav" -map "[out2]" "%output_dir%\%%~nF_part2.wav"
+    setlocal enabledelayedexpansion
+    for /F "usebackq" %%A in (`ffprobe -v error -show_entries format^=duration -of default^=noprint_wrappers^=1:nokey^=1 "%%F"`) do (
+        set "duration=%%A"
+        set /A "half_duration=!duration!/2"
+    )
+
+    echo [ * ] Duration: !duration!
+    echo [ * ] Half Duration: !half_duration!
+
+    ffmpeg -i "%%F" -ss 0 -t !half_duration! -c copy "%output_dir%\%%~nF_part1.wav"
+    ffmpeg -i "%%F" -ss !half_duration! -c copy "%output_dir%\%%~nF_part2.wav"
+    endlocal
 )
--- scripts/service/video_process.py
+++ scripts/service/video_process.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 
 def convert_mp4_to_wav(mp4_file, wav_file):
@@ -14,4 +15,37 @@ def convert_mp4_to_wav(mp4_file, wav_file):
     os.system(cmd)
 
 
+def duration_get(file_path):
+    ffprobe_command = f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "{file_path}"'
+    result = subprocess.check_output(ffprobe_command, shell=True, universal_newlines=True)
+    duration = float(result)
+    return duration
 
+
+def duration_split(input_dir, output_dir, num_parts=2, file_ext=".wav"):
+    if not input_dir:
+        print("Input directory not provided.")
+        return
+
+    if not output_dir:
+        output_dir = os.path.join(input_dir, "output")
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    for file_name in os.listdir(input_dir):
+        if file_name.endswith(file_ext):
+            file_path = os.path.join(input_dir, file_name)
+            duration = duration_get(file_path)
+            part_duration = duration / num_parts
+
+            print("[ * ] Duration:", duration)
+            print("[ * ] Number of Parts:", num_parts)
+            print("[ * ] Part Duration:", part_duration)
+
+            base_name = os.path.splitext(file_name)[0]
+
+            for i in range(num_parts):
+                part_output_file = os.path.join(output_dir, f"{base_name}_part{i + 1}{file_ext}")
+                start_time = i * part_duration
+                ffmpeg_split_command = f'ffmpeg -i "{file_path}" -ss {start_time} -t {part_duration} -c copy "{part_output_file}"'
+                subprocess.call(ffmpeg_split_command, shell=True)
