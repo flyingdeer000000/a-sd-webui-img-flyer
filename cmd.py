4ed33f56b1720b826963eee28f6def91d0f25296
kaar <flyingdeer000000@gmail.com>
2024-05-03 12:44:46 +0800

    commit

---
+++ cmd.py
@@ -0,0 +1,181 @@
+import argparse
+import os
+
+from scripts.service import video_process, util, image_process
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Image processing script')
+
+    # parser.add_argument('--cmd', dest='cmd', required=True, help='command to perform')
+
+    subparsers = parser.add_subparsers(dest='subcmd')
+
+    # img
+    img_parser = subparsers.add_parser('img', help='perform image processing')
+    img_parser.add_argument('--src-dir', dest='src_dir', help='source directory')
+    img_parser.add_argument('--des-dir',
+                            dest='des_dir',
+                            help='target directory (default: output)')
+    img_parser.add_argument('--resize',
+                            dest='resize',
+                            default='512x512',
+                            help='resize size (default: 512x512)')
+    img_parser.add_argument('--resize-fill',
+                            dest='resize_fill_color',
+                            type=str, default='',
+                            help='resize fill color')
+    img_parser.add_argument('--resize-remove',
+                            dest='resize_remove_color',
+                            type=str, default='',
+                            help='resize remove color')
+    img_parser.add_argument('--rembg-model',
+                            dest='rembg_model',
+                            type=str, default='isnet-anime',
+                            help='remove background model (default: isnet-anime)')
+    img_parser.add_argument('--rembg-color',
+                            dest='rembg_color',
+                            type=str, default='',
+                            help='padding color after remove background (default=transparent)')
+    img_parser.add_argument('--depth',
+                            dest='dir_depth',
+                            type=int, default=0,
+                            help='recursive depth (default: 0)')
+
+    # to_wav
+    wav_parser = subparsers.add_parser('2wav', aliases=['to_wav'], help='convert video to WAV')
+    wav_parser.add_argument('--src-file', dest='src_file', help='source file')
+    wav_parser.add_argument('--des-file', dest='des_file', help='target file')
+
+    # split
+    split_parser = subparsers.add_parser('split',
+                                         help='split videos based on duration')
+    split_parser.add_argument('--src-dir',
+                              dest='src_dir',
+                              help='source directory')
+    split_parser.add_argument('--des-dir',
+                              dest='des_dir',
+                              type=str, default="",
+                              help='target directory (default: output)')
+    split_parser.add_argument('--divider',
+                              dest='divider',
+                              type=int, default=2,
+                              help='divider (default: 2)')
+    split_parser.add_argument('--file-ext',
+                              dest='file_ext',
+                              type=str, default='wav',
+                              help='file extension (default: wav')
+
+    # sum_duration
+    sum_parser = subparsers.add_parser('sum_duration', help='sum directory media file duration')
+    sum_parser.add_argument('--dir', required=True, dest='directory', help='directory path')
+
+    return parser.parse_args()
+
+
+def to_wav(args):
+    if args.src_file is None:
+        raise Exception("missing argument --src-file")
+
+    if args.des_file is None:
+        args.des_file = args.src_file.replace(".mp4", ".wav")
+
+    print("[src_file] source: {}".format(args.src_file))
+    print("[des_file] target: {}".format(args.des_file))
+    video_process.convert_mp4_to_wav(args.src_file, args.des_file)
+
+
+def split(args):
+    if args.src_dir is None:
+        raise Exception("missing argument --src-dir")
+
+    if args.des_dir is None:
+        args.des_dir = os.path.join(args.src_dir, 'output')
+
+    args.divider = int(args.divider)
+    args.file_ext = str(args.file_ext).lower()
+
+    video_process.duration_split(args.src_dir, args.des_dir, args.divider, args.file_ext)
+
+
+def sum_duration(args):
+    if args.directory is None:
+        raise Exception("missing argument --dir")
+
+    total_sec = video_process.duration_sum(args.directory)
+    time = util.format_time(total_sec)
+    print("Total Duration: {}".format(time))
+
+
+def img_process(args):
+    resize_width, resize_height = map(int, args.resize_image.split('x'))
+
+    if args.rembg_model.lower() == 'resize':
+        rembg_model = ''
+    else:
+        rembg_model = args.rembg_model
+
+    args.resize_fill_color = getattr(args, 'resize_fill_color', '0,0,0,0')
+    args.resize_remove_color = getattr(args, 'resize_remove_color', '')
+    args.rembg_color = getattr(args, 'rembg_color', '0,0,0,0')
+    args.dir_depth = getattr(args, 'dir_depth', 0)
+
+    print("[directory] source: {}".format(args.src_dir))
+    print("[directory] target: {}".format(args.des_dir))
+    print("[resize] size: {} x {} | fill color: {} | remove color: {}".format(
+        resize_width, resize_height,
+        args.resize_fill_color, args.resize_remove_color
+    ))
+    print("[remove background] model: {} | color: {}".format(rembg_model, args.rembg_color))
+    print("[recursive] depth: {}".format(args.dir_depth))
+
+    if not os.path.exists(args.src_dir):
+        print("[directory] not found: {}".format(args.src_dir))
+        exit(404)
+
+    if args.des_dir is None:
+        des_dir = os.path.join(args.src_dir, 'output')
+    else:
+        des_dir = args.des_dir
+
+    image_process.process(
+        src_dir=args.src_dir,
+        des_dir=des_dir,
+        resize_width=resize_width,
+        resize_height=resize_height,
+        resize_fill_color=args.resize_fill_color,
+        resize_remove_color=args.resize_remove_color,
+        rembg_model=rembg_model,
+        rembg_color=args.rembg_color,
+        recursive_depth=args.dir_depth,
+    )
+
+
+def testing(a):
+    a.subcmd = 'img'
+    a.src_dir = './1'
+    a.des_dir = './2'
+    a.resize_image = '512x768'
+    a.resize_remove_color = 'auto'
+    # a.resize_remove_color = '255,13,85,256'
+    a.rembg_model = 'none'
+
+    return a
+
+
+if __name__ == '__cmd__':
+    args = parse_arguments()
+
+    # args = testing(args)
+
+    print("[cmd] {}".format(args.subcmd))
+
+    if args.subcmd == 'img':
+        img_process(args)
+    elif args.subcmd == '2wav' or args.subcmd == 'to_wav':
+        to_wav(args)
+    elif args.subcmd == 'split':
+        split(args)
+    elif args.subcmd == 'sum_duration':  # Handle the sum subcommand
+        sum_duration(args)
+        print("[fin]")
--- main.py
+++ main.py
@@ -1,183 +1,121 @@
+import io
 import os
-import argparse
+import sys
+
 from scripts.service import image_process, video_process, util
+import gradio as gr
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Image processing script')
+def capture_console_output(func):
+    def wrapper(*args, **kwargs):
+        original_stdout = sys.stdout
+        original_stderr = sys.stderr
+        sys.stdout = io.StringIO()  # Redirect stdout
+        sys.stderr = io.StringIO()  # Redirect stderr
+        try:
+            result = func(*args, **kwargs)
+            output = sys.stdout.getvalue()
+            error = sys.stderr.getvalue()
+            return result + "\n" + output + error
+        finally:
+            sys.stdout = original_stdout  # Reset stdout to original
+            sys.stderr = original_stderr  # Reset stderr to original
 
-    # parser.add_argument('--cmd', dest='cmd', required=True, help='command to perform')
-
-    subparsers = parser.add_subparsers(dest='subcmd')
-
-    # img
-    img_parser = subparsers.add_parser('img', help='perform image processing')
-    img_parser.add_argument('--src-dir', dest='src_dir', help='source directory')
-    img_parser.add_argument('--des-dir',
-                            dest='des_dir',
-                            help='target directory (default: output)')
-    img_parser.add_argument('--resize',
-                            dest='resize',
-                            default='512x512',
-                            help='resize size (default: 512x512)')
-    img_parser.add_argument('--resize-fill',
-                            dest='resize_fill_color',
-                            type=str, default='',
-                            help='resize fill color')
-    img_parser.add_argument('--resize-remove',
-                            dest='resize_remove_color',
-                            type=str, default='',
-                            help='resize remove color')
-    img_parser.add_argument('--rembg-model',
-                            dest='rembg_model',
-                            type=str, default='isnet-anime',
-                            help='remove background model (default: isnet-anime)')
-    img_parser.add_argument('--rembg-color',
-                            dest='rembg_color',
-                            type=str, default='',
-                            help='padding color after remove background (default=transparent)')
-    img_parser.add_argument('--depth',
-                            dest='dir_depth',
-                            type=int, default=0,
-                            help='recursive depth (default: 0)')
-
-    # to_wav
-    wav_parser = subparsers.add_parser('2wav', aliases=['to_wav'], help='convert video to WAV')
-    wav_parser.add_argument('--src-file', dest='src_file', help='source file')
-    wav_parser.add_argument('--des-file', dest='des_file', help='target file')
-
-    # split
-    split_parser = subparsers.add_parser('split',
-                                         help='split videos based on duration')
-    split_parser.add_argument('--src-dir',
-                              dest='src_dir',
-                              help='source directory')
-    split_parser.add_argument('--des-dir',
-                              dest='des_dir',
-                              type=str, default="",
-                              help='target directory (default: output)')
-    split_parser.add_argument('--divider',
-                              dest='divider',
-                              type=int, default=2,
-                              help='divider (default: 2)')
-    split_parser.add_argument('--file-ext',
-                              dest='file_ext',
-                              type=str, default='wav',
-                              help='file extension (default: wav')
-
-    # sum_duration
-    sum_parser = subparsers.add_parser('sum_duration', help='sum directory media file duration')
-    sum_parser.add_argument('--dir', required=True, dest='directory', help='directory path')
-
-    return parser.parse_args()
+    return wrapper
 
 
-def img_process(args):
-    resize_width, resize_height = map(int, args.resize_image.split('x'))
-
-    if args.rembg_model.lower() == 'resize':
-        rembg_model = ''
-    else:
-        rembg_model = args.rembg_model
-
-    args.resize_fill_color = getattr(args, 'resize_fill_color', '0,0,0,0')
-    args.resize_remove_color = getattr(args, 'resize_remove_color', '')
-    args.rembg_color = getattr(args, 'rembg_color', '0,0,0,0')
-    args.dir_depth = getattr(args, 'dir_depth', 0)
-
-    print("[directory] source: {}".format(args.src_dir))
-    print("[directory] target: {}".format(args.des_dir))
-    print("[resize] size: {} x {} | fill color: {} | remove color: {}".format(
-        resize_width, resize_height,
-        args.resize_fill_color, args.resize_remove_color
-    ))
-    print("[remove background] model: {} | color: {}".format(rembg_model, args.rembg_color))
-    print("[recursive] depth: {}".format(args.dir_depth))
-
-    if not os.path.exists(args.src_dir):
-        print("[directory] not found: {}".format(args.src_dir))
-        exit(404)
-
-    if args.des_dir is None:
-        des_dir = os.path.join(args.src_dir, 'output')
-    else:
-        des_dir = args.des_dir
+@capture_console_output
+def img_process_interface(src_dir, des_dir, resize, resize_fill_color, resize_remove_color, rembg_model, rembg_color,
+                          dir_depth):
+    # Convert string resize '512x512' into two integers
+    resize_width, resize_height = map(int, resize.split('x'))
+    # Check if directories exist, if not create
+    if not os.path.exists(des_dir):
+        os.makedirs(des_dir)
 
     image_process.process(
-        src_dir=args.src_dir,
+        src_dir=src_dir,
         des_dir=des_dir,
         resize_width=resize_width,
         resize_height=resize_height,
-        resize_fill_color=args.resize_fill_color,
-        resize_remove_color=args.resize_remove_color,
+        resize_fill_color=resize_fill_color,
+        resize_remove_color=resize_remove_color,
         rembg_model=rembg_model,
-        rembg_color=args.rembg_color,
-        recursive_depth=args.dir_depth,
+        rembg_color=rembg_color,
+        recursive_depth=dir_depth,
+    )
+    return f"Processed images are saved in {des_dir}"
+
+
+@capture_console_output
+def to_wav_interface(src_file, des_file):
+    video_process.convert_mp4_to_wav(src_file, des_file)
+    return f"Converted WAV file is saved as {des_file}"
+
+
+@capture_console_output
+def split_interface(src_dir, des_dir, divider, file_ext):
+    video_process.duration_split(src_dir, des_dir, divider, file_ext)
+    return f"Videos split successfully into {des_dir}"
+
+
+@capture_console_output
+def sum_duration_interface(directory):
+    total_sec = video_process.duration_sum(directory)
+    time = util.format_time(total_sec)
+    return f"Total Duration: {time}"
+
+
+def webui(port):
+    with gr.Blocks() as demo:
+        with gr.Tab("Image Processing"):
+            with gr.Row():
+                src_dir = gr.Textbox(label="Source Directory")
+            with gr.Row():
+                des_dir = gr.Textbox(label="Destination Directory")
+            with gr.Row():
+                resize = gr.Textbox(value="512x512", label="Resize (e.g., 512x512)")
+                resize_fill_color = gr.Textbox(label="Resize Fill Color")
+                resize_remove_color = gr.Textbox(label="Resize Remove Color")
+                rembg_model = gr.Textbox(label="Remove Background Model")
+                rembg_color = gr.Textbox(label="Remove Background Color")
+                dir_depth = gr.Number(label="Directory Depth", value=0)
+            with gr.Row():
+                run_img = gr.Button("Run Image Processing")
+
+            img_output = gr.Textbox(label="Output")
+            run_img.click(img_process_interface,
+                          inputs=[src_dir, des_dir, resize, resize_fill_color, resize_remove_color, rembg_model,
+                                  rembg_color, dir_depth], outputs=img_output)
+
+        with gr.Tab("Video to WAV"):
+            src_file = gr.Textbox(label="Source Video File")
+            des_file = gr.Textbox(label="Destination WAV File")
+            run_wav = gr.Button("Convert to WAV")
+            wav_output = gr.Textbox(label="Output")
+            run_wav.click(to_wav_interface, inputs=[src_file, des_file], outputs=wav_output)
+
+        with gr.Tab("Split Video"):
+            split_src_dir = gr.Textbox(label="Source Directory")
+            split_des_dir = gr.Textbox(label="Destination Directory")
+            divider = gr.Number(label="Divider", value=2)
+            file_ext = gr.Textbox(label="File Extension", value="wav")
+            run_split = gr.Button("Split Video")
+            split_output = gr.Textbox(label="Output")
+            run_split.click(split_interface, inputs=[split_src_dir, split_des_dir, divider, file_ext],
+                            outputs=split_output)
+
+        with gr.Tab("Sum Duration"):
+            sum_dir = gr.Textbox(label="Directory")
+            run_sum = gr.Button("Calculate Duration")
+            sum_output = gr.Textbox(label="Output")
+            run_sum.click(sum_duration_interface, inputs=[sum_dir], outputs=sum_output)
+
+    demo.launch(
+        server_port=port,
+        debug=True
     )
 
 
-def to_wav(args):
-    if args.src_file is None:
-        raise Exception("missing argument --src-file")
-
-    if args.des_file is None:
-        args.des_file = args.src_file.replace(".mp4", ".wav")
-
-    print("[src_file] source: {}".format(args.src_file))
-    print("[des_file] target: {}".format(args.des_file))
-    video_process.convert_mp4_to_wav(args.src_file, args.des_file)
-
-
-def split(args):
-    if args.src_dir is None:
-        raise Exception("missing argument --src-dir")
-
-    if args.des_dir is None:
-        args.des_dir = os.path.join(args.src_dir, 'output')
-
-    args.divider = int(args.divider)
-    args.file_ext = str(args.file_ext).lower()
-
-    video_process.duration_split(args.src_dir, args.des_dir, args.divider, args.file_ext)
-
-
-def sum_duration(args):
-    if args.directory is None:
-        raise Exception("missing argument --dir")
-
-    total_sec = video_process.duration_sum(args.directory)
-    time = util.format_time(total_sec)
-    print("Total Duration: {}".format(time))
-
-
-def testing(a):
-
-    a.subcmd = 'img'
-    a.src_dir = 'D:/work/ai/models/psplive/dousha/school/512x768_white/1'
-    a.des_dir = 'D:/work/ai/models/psplive/dousha/school/512x768_white/2'
-    a.resize_image = '512x768'
-    a.resize_remove_color = 'auto'
-    # a.resize_remove_color = '255,13,85,256'
-    a.rembg_model = 'none'
-
-    return a
-
-
 if __name__ == '__main__':
-
-    args = parse_arguments()
-
-    args = testing(args)
-
-    print("[cmd] {}".format(args.subcmd))
-
-    if args.subcmd == 'img':
-        img_process(args)
-    elif args.subcmd == '2wav' or args.subcmd == 'to_wav':
-        to_wav(args)
-    elif args.subcmd == 'split':
-        split(args)
-    elif args.subcmd == 'sum_duration':  # Handle the sum subcommand
-        sum_duration(args)
-
-    print("[fin]")
+    webui(10005)
